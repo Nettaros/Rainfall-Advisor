7dc56d2508d2c6d6c35a48dfa1c8dad459ee41f0
Martin Moschettoni <moschettonimartin@hotmail.com>
2022-06-14 23:18:01 -0300

    Agrega funcionalidad de tabs en recomendaciones

--- DEU_App/src/components/screens/RecommendationsScreen.jsx
+++ DEU_App/src/components/screens/RecommendationsScreen.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, ScrollView} from 'react-native'
+import {createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer }  from '@react-navigation/native';
+import Appbar from '../AppBar.jsx'
+import RecoBeforeScreen from './RecoBefore.jsx'
+import RecoDuringScreen from './RecoDuringScreen.jsx'
+import RecoAfterScreen from './RecoAfterScreen.jsx'
 
 const RecommendationsScreen = () => {
+    const Tab = createMaterialTopTabNavigator()
     return (
-        <View>
-            <Text>
-                Recomendaciones
-            </Text>
-        </View>
+        <NavigationContainer independent={true}>
+            <View>
+                <Tab.Navigator>
+                    <Tab.Screen name="Antes" component={RecoBeforeScreen}/>
+                    <Tab.Screen name="Durante" component={RecoDuringScreen}/>
+                    <Tab.Screen name="Después" component={RecoAfterScreen}/>
+                </Tab.Navigator>
+            </View>
+        </NavigationContainer>
     )
 }
 
 export default RecommendationsScreen
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: "#fafaf"
+    },
+    title: {
+        fontSize: 32,
+        fontStyle: "bold",
+        alignSelf:"center"
+    },
+    bar: {
+        fontSize: 20
+    }
+})
