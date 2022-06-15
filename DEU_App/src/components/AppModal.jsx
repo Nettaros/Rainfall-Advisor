b32c2f9fff4fe4c806c380a4a32554a84a20f043
Martin Moschettoni <moschettonimartin@hotmail.com>
2022-06-14 23:25:05 -0300

    Agrega nuevos archivos para construir la funcionalidad de configuración

---
+++ DEU_App/src/components/AppModal.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {View, TouchableOpacity, Text, Modal, ScrollView} from 'react-native'
+
+const AppModal = ({modalVisible, setModalVisible, modalBody, title}) => {
+    return (
+        <Modal visible={modalVisible} animationType="slide">
+                <View style={{backgroundColor: "#fff"}}>
+                    <ScrollView>
+                        <View style={{
+                            flexDirection: "row",
+                            padding: 15,
+                            alignItems: "center",
+                            width: "100%",
+                            justifyContent: "space-between"
+                        }}>
+                            <TouchableOpacity onPress={() => {
+                                setModalVisible(false)
+                            }}>
+                                <Text fontSize={30}>X</Text>
+                            </TouchableOpacity>
+                            <Text style={{fontSize: 20}}>{title || "Titulo"}</Text>
+                            <Text/>
+                            <Text/>
+                            <Text/>
+                            <Text/>
+
+                        </View>
+                        <View style={{height:1, backgroundColor: "#4f4f4f"}} />
+                        <View>
+                            {modalBody}
+                        </View>
+                    </ScrollView>
+                </View>
+        </Modal>
+    )
+}
+
+export default AppModal
