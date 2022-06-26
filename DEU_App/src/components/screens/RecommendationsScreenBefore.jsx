2c821300480b5f1ff1c215f96a85155b52ec68f8
Martin Moschettoni <moschettonimartin@hotmail.com>
2022-06-26 19:35:05 -0300

    Agrega texto real a recomendaciones y arregla problema con la API

--- DEU_App/src/components/screens/RecommendationsScreenBefore.jsx
+++ DEU_App/src/components/screens/RecommendationsScreenBefore.jsx
@@ -10,10 +10,10 @@ const RecoBeforeScreen = () => {
     return (
         <View accessible={true}>
             <ScrollView>
-                <TabListElement text="Recom 1"/>
-                <TabListElement text="Recom 2"/>
-                <TabListElement text="Recom 3"/>
-                <TabListElement text="Recom 4"/>
+            <TabListElement text=
+                    "Recogé la basura, escombros, ramas y hojas de tu casa para evitar que obstaculicen el paso del agua durante lluvias intensas y desborde de arroyos"/>
+                <TabListElement text="Revisá periódicamente el estado de techos, canaletas, rejillas y desagües pluviales de tu casa. Sellá posibles fultraciones con algún material impermeable"/>
+                <TabListElement text="Identificá que muebles o ropa pueden verse afectados por el agua. Planificá como podrías elevarlos en caso de precisarlo y de ser posible, adelantate y colocalos en un lugar alto."/>
             </ScrollView>
         </View>
     )
--- DEU_App/src/components/screens/RecommendationsScreenDuring.jsx
+++ DEU_App/src/components/screens/RecommendationsScreenDuring.jsx
@@ -10,10 +10,10 @@ const RecoDuringScreen = () => {
     return (
         <View accessible={true}>
             <ScrollView>
-                <TabListElement text="Recom 1"/>
-                <TabListElement text="Recom 2"/>
-                <TabListElement text="Recom 3"/>
-                <TabListElement text="Recom 4"/>
+                <TabListElement text="Si estas caminando mantenete alejado de tendidos eléctricos, postes caídos o cables, ya que podrías electrocutarte"/>
+                <TabListElement text="Si estas en un lugar cerrado cortá la electricidad y el gas."/>
+                <TabListElement text="Ubicate en zonas altas, la planta superior del lugar o el techo."/>
+                <TabListElement text="Si tenés una casa de dos plantas, una terraza o acceso al techo y ves que hay personas corriendo peligro en la calle, ofreceles resguardo."/>
             </ScrollView>
         </View>
     )
