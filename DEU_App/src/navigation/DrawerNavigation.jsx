85fbe65291f59674365021b4f42feb6c07cd1ffa
Martin Moschettoni <moschettonimartin@hotmail.com>
2022-06-26 19:08:26 -0300

    Regresa cambio en accesibilidad porque la app dejo de funcionar

--- DEU_App/src/navigation/DrawerNavigation.jsx
+++ DEU_App/src/navigation/DrawerNavigation.jsx
@@ -17,14 +17,12 @@ export default function DrawerNavigation() {
     const {colors} = useTheme();
     
     return (
-        <View accessible={true}>
-            <Drawer.Navigator accessibilityRole="menu" screenOptions={{headerTintColor: colors.text}} drawerContent={(props) => <MenuItem {...props} />}>
-                <Drawer.Screen name="Precipitación" component={MainScreen}/>
-                <Drawer.Screen name="Configuración" component={SettingScreen}/>
-                <Drawer.Screen name="Recomendaciones" component={RecommendationsScreen}/>
-                <Drawer.Screen name="Sobre la App" component={AboutScreen}/>
-            </Drawer.Navigator>
-        </View>
+        <Drawer.Navigator accessible={true} accessibilityRole="menu" screenOptions={{headerTintColor: colors.text}} drawerContent={(props) => <MenuItem {...props} />}>
+            <Drawer.Screen name="Precipitación" component={MainScreen}/>
+            <Drawer.Screen name="Configuración" component={SettingScreen}/>
+            <Drawer.Screen name="Recomendaciones" component={RecommendationsScreen}/>
+            <Drawer.Screen name="Sobre la App" component={AboutScreen}/>
+        </Drawer.Navigator>
     );
 }
 
